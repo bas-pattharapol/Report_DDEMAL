fd1fa91897257c271d51867802288ac9569aada9
Pattharapol Choonarong <baskzxcpatt@gmail.com>
2022-02-21 10:20:02 +0700

    master

--- web/excelToSQL.py
+++ web/excelToSQL.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pyodbc
 
-data = pd.read_csv (r'D:\Work\foster\Report_DDEMAL\web\Book.csv')   
+data = pd.read_csv (r'D:\Work\foster\Report_DDEMAL\web\test.csv')   
 df = pd.DataFrame(data)
 
 print(df)
@@ -13,8 +13,8 @@ password = "p@ssw0rd"
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+ server +';DATABASE='+database+';UID='+username+';PWD='+password)
 cursor = cnxn.cursor()
 for row in df.itertuples():
-    postgres_insert_query = 'INSERT INTO SCADA_DB.dbo.Phase_Parameter (PhaseID, PhaseName,Description ,Parameter1, Parameter2, Parameter3, Parameter4, Parameter5, Parameter6, Parameter7) VALUES (?,?,?,?,?,?,?,?,?,?)'
-    record_to_insert = (row.Phase_ID, row.Phase_Name,row.Description,row.Parameter1,row.Parameter2,row.Parameter3,row.Parameter4,row.Parameter5,row.Parameter6,row.Parameter7)
+    postgres_insert_query = 'INSERT INTO SCADA_DB.dbo.Phase_Parameter (PhaseID, PhaseName,Description ,Parameter1, Parameter2, Parameter3, Parameter4, Parameter5, Parameter6, Parameter7,Parameter8) VALUES (?,?,?,?,?,?,?,?,?,?,?)'
+    record_to_insert = (int(row.Phase_ID), str(row.Phase_Name),str(row.Description),str(row.Parameter1),str(row.Parameter2),str(row.Parameter3),str(row.Parameter4),str(row.Parameter5),str(row.Parameter6),str(row.Parameter7),str(row.Parameter8))
     cursor.execute(postgres_insert_query, record_to_insert)
                     
                     
