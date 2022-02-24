61b17315745105142cdbbdfa038cd71c3544c60b
Pattharapol Choonarong <baskzxcpatt@gmail.com>
2022-02-24 16:56:06 +0700

    master

--- web/app.py
+++ web/app.py
@@ -86,7 +86,7 @@ def modeReport(pdOrder):
         insertObject.append( dict( zip( columnNames , record ) ) )
     print(insertObject)
     
-    return render_template('report.html',pdOrder=pdOrder,Phase_Parameter=insertObject,ST_Report=ST_Report,len=len(Phase_Parameter_DIR))
+    return render_template('report.html',pdOrder=pdOrder,Phase_Parameter=insertObject,ST_Report=ST_Report,ST_Report1=ST_Report,len=len(Phase_Parameter_DIR),len1=len(Phase_Parameter_DIR))
  
 @app.route('/batch_report_API' ,methods=["GET", "POST"])
 def batch_report_API():
